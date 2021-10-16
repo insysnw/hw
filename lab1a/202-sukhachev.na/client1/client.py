68761a51b40a08f04b49f9d4a1e3e7f507430f60
Nikita <traed.traed@mail.ru>
2021-10-16 16:36:51 +0300

    initial commit

---
+++ lab1a/202-sukhachev.na/client1/client.py
@@ -0,0 +1,102 @@
+import threading
+import socket
+from datetime import datetime
+import signal
+import os
+import re
+
+HEADER_LENGTH = 10
+SEPARATOR = "<SEPARATOR>"
+SEND_FILE = 2**72
+byteorder = "big"
+
+IP = "127.0.0.1"
+PORT = 10000
+
+CONNECT = "CONNECT"
+DISCONNECT = "DISCONNECT"
+CONNECTION = 2**64
+
+
+def main():
+    nickname = input("Напишите имя для чата: ")
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((IP, PORT))
+
+    def catch_interrupt(signal, frame):
+        clientsocket.shutdown(socket.SHUT_WR)
+        clientsocket.close()
+        os._exit(0)
+
+    signal.signal(signal.SIGINT, catch_interrupt)
+    nickname_code = nickname.encode('utf-8')
+    nickname_header = len(nickname_code).to_bytes(HEADER_LENGTH,byteorder)
+    clientsocket.send(nickname_header + nickname_code)
+    receive_thread = threading.Thread(target=getMessage, args=(clientsocket,))
+    receive_thread.start()
+    try:
+        while True:
+            message = input()
+            if (re.match("^send .*\.\w*\s*$", message)):
+                try:
+                    files = re.findall("\s+\w.*\.\w*\s*", message)
+                    fileName = files[0].strip()
+                    filesize = os.path.getsize(fileName)
+                    fileHeader = f"{files[0]}{SEPARATOR}{filesize}".encode()
+                    clientsocket.send((SEND_FILE+len(fileHeader)).to_bytes(HEADER_LENGTH,byteorder))
+                    clientsocket.send(fileHeader)
+                    f = open(fileName, "rb")
+                    bytes_read = f.read(filesize)
+                    clientsocket.sendall(bytes_read)
+                    f.close()
+                except:
+                    print(f'Невозможно найти файл {fileName}')
+
+            else:
+                if message:
+                    message_code = message.encode('utf-8')
+                    message_header = len(message_code).to_bytes(HEADER_LENGTH,byteorder)
+                    clientsocket.send(
+                        message_header + message_code )
+    except:
+        os._exit(0)
+
+
+def getMessage(clientsocket):
+    while True:
+        nickname_header = clientsocket.recv(HEADER_LENGTH)
+        nickname_length = int.from_bytes(nickname_header, byteorder)
+        current_time = datetime.now().strftime("%H:%M")
+        if len(nickname_header) == 0:
+            print("Закрываю соединение")
+            clientsocket.shutdown(socket.SHUT_WR)
+            clientsocket.close()
+            os._exit(0)
+        if nickname_length > SEND_FILE:
+            name = clientsocket.recv(nickname_length- SEND_FILE).decode()
+            file_header = clientsocket.recv(HEADER_LENGTH)
+            file_header_len = int.from_bytes(file_header,byteorder)
+            file_header = clientsocket.recv(file_header_len)
+            filename, filesize = file_header.decode('utf-8').split(SEPARATOR)
+            filename = os.path.basename(filename)
+            filesize = int(filesize)
+            f = open(filename, "wb")
+            bytes_read = clientsocket.recv(filesize)
+            f.write(bytes_read)
+            f.close()
+            print(f'<{current_time}> {name} отправляет файл {filename}')
+        else:
+            if nickname_length > CONNECTION:
+                warning = clientsocket.recv(nickname_length - CONNECTION).decode('utf-8')
+                print(f'{warning}')
+                continue
+            nickname = clientsocket.recv(nickname_length).decode('utf-8')
+            message_header = clientsocket.recv(HEADER_LENGTH)
+            message_length = int.from_bytes(message_header, byteorder)
+            message = clientsocket.recv(message_length).decode('utf-8')
+            print(f'<{current_time}> [{nickname}]: {message}')
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ lab1a/202-sukhachev.na/server/server.py
@@ -0,0 +1,133 @@
+import socket
+import threading
+from datetime import datetime
+import os
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 10000
+clients = {}
+SEND_FILE = 2 ** 72
+SEPARATOR = "<SEPARATOR>"
+byteorder = "big"
+CONNECT = "CONNECT"
+DISCONNECT = "DISCONNECT"
+CONNECTION = 2**64
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen(5)
+    print("Сервер запущен!!!")
+    try:
+        while True:
+            (clientsocket, address) = server.accept()
+            client = getMessage(clientsocket)
+            if client:
+                if client in clients.values():
+                    notice = f"Извините, клиент с именем {client['data'].decode('UTF-8')} уже существует," \
+                             f"выберите другое имя".encode('utf-8')
+                    notice_header = int.to_bytes(CONNECTION+len(notice),HEADER_LENGTH,byteorder)
+                    message =  notice_header + notice
+                    clientsocket.send(message)
+                    clientsocket.close()
+                else:
+                    clients[clientsocket] = client
+                    time = datetime.now().strftime("%H:%M")
+                    name = client['data'].decode('UTF-8')
+                    print(
+                        f"В {time} подключился новый клиент {name}")
+                    notificationForClient(CONNECT, clientsocket)
+                    handler_thread = threading.Thread(target=handle_client,
+                                                      args=(clientsocket,))
+                    handler_thread.start()
+    except KeyboardInterrupt:
+        for cl in clients:
+            cl.shutdown(socket.SHUT_WR)
+            cl.close()
+        server.shutdown(socket.SHUT_WR)
+        server.close()
+        os._exit(0)
+
+
+def sendToAll(msg, clientsocket):
+    for client in clients:
+        if client != clientsocket:
+            client.send(msg)
+
+
+def notificationForClient(type, clientsocket):
+    code_n = 2 ** 64
+    notice: bytes
+    name = clients[clientsocket]['data'].decode('utf-8')
+    if type == CONNECT:
+        notice = f"{name} входит в чат ".encode('utf-8')
+    if type == DISCONNECT:
+        notice = f"{name} выходит из чата ".encode('utf-8')
+    notice_header = (code_n + len(notice)).to_bytes(HEADER_LENGTH, byteorder)
+    message = notice_header + notice
+    sendToAll(message, clientsocket)
+
+
+def getMessage(clientsocket):
+    try:
+        message_header = clientsocket.recv(HEADER_LENGTH)
+        messsage_length = int.from_bytes(message_header, byteorder)
+        if not len(message_header):
+            return False
+        if messsage_length > SEND_FILE:
+            file_header = clientsocket.recv(messsage_length - SEND_FILE)
+            filename, filesize = file_header.decode('utf-8').split(
+                SEPARATOR)
+            filesize = int(filesize)
+            bytes_read = clientsocket.recv(filesize)
+            return {
+                'sendFile': True,
+                'header': file_header,
+                'data': bytes_read
+            }
+        else:
+            return {
+                'sendFile': False,
+                'header': message_header,
+                'data': clientsocket.recv(messsage_length)}
+    except ValueError:
+        print("Тип header должен быть int")
+        return False
+    except:
+        return False
+
+
+def handle_client(clientsocket):
+    while True:
+        message = getMessage(clientsocket)
+        name = clients[clientsocket]['data'].decode('utf-8')
+        current_time = datetime.now().strftime("%H:%M")
+        if message is False:
+            clientsocket.shutdown(socket.SHUT_WR)
+            clientsocket.close()
+            print(f"{name} отключается")
+            notificationForClient(DISCONNECT, clientsocket)
+            del clients[clientsocket]
+            return None
+        if message['sendFile']:
+            message_header_len = int.to_bytes(len(message["header"]),HEADER_LENGTH,byteorder)
+            name_header = (SEND_FILE+int.from_bytes((clients[clientsocket]['header']),byteorder)).to_bytes(HEADER_LENGTH,byteorder)
+            fileMessage = name_header + clients[clientsocket][
+                'data'] + message_header_len + \
+                          message["header"] + message["data"]
+            sendToAll(fileMessage, clientsocket)
+            filename = message["header"].decode('utf-8').split(SEPARATOR)[0]
+            print(
+                f"В {current_time} клиент {name} отправил файл {filename}")
+        else:
+            print(
+                f'В {current_time}  получено сообщение от {name}: {message["data"].decode("utf-8")}')
+            final_message = clients[clientsocket]['header'] + clients[clientsocket]['data'] + message['header'] + \
+                            message['data']
+            sendToAll(final_message, clientsocket)
+
+
+if __name__ == '__main__':
+    main()
