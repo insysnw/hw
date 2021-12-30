06378ad1174ba91784d5b84a0d42a6f03c923dba
FenstonSingel <fenstonsingel@gmail.com>
2021-12-30 17:52:45 +0300

    Added necessary JUnit dependencies

--- lab1b/202-ruban.sd/build.gradle.kts
+++ lab1b/202-ruban.sd/build.gradle.kts
@@ -13,4 +13,13 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
