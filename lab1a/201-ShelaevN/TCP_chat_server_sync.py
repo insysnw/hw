f604a2e3173c0495ec6e97fb65f8ab4cdfeb2891
Никита Шелаев <42943483+ShelaevNikita@users.noreply.github.com>
2022-01-06 21:29:24 +0300

    Deleting clients when connection is lost

--- lab1a/201-ShelaevN/TCP_chat_server_sync.py
+++ lab1a/201-ShelaevN/TCP_chat_server_sync.py
@@ -4,109 +4,111 @@ import threading
 
 from datetime import timezone, datetime
 
+PORT = 7575
+ENCODING = 'utf-8'
+
 '''
-Protocol:
+TCP_Protocol:
     Time(5 + 6 + 6 = 17) | Length_name(6) | Flag_file(1) | Name() | Message()
 '''
 
 class TCP_chat_sync_server():
 
-    def __init__(self):
-        self.FILE_SPLIT = '?/:'
+    def __init__(self, PORT, ENCODING):
 
         self.SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.SOCKET.bind(('', 7575))
+        self.SOCKET.bind(('', PORT))
         self.SOCKET.listen(5)
 
-        self.CLIENTS = {'TCP-server':'-'}
+        self.NAME = 'TCP-server'
+        self.CLIENTS = {self.NAME:'-'}
+
+        self.ENCODING = ENCODING
         self.main()
 
-    def bitToInt(self, data):
-        result = 0
-        for i in range(0, len(data)):
-            result = result * 2 + int(data[i])
-        return result
+    def message2Bit(self, message, lenght):
+        return format(message, 'b').zfill(lenght)
 
-    def byte_decode(self, data_bin):
-        res_str = ''
-        for i in range(0, len(data_bin), 8):
-            res_str += int(data_bin[i:(i + 8)], 2).to_bytes(1, byteorder = 'big').decode('utf-8') 
-        return res_str
-
-    def bit_zfill(self, mess, length):
-        return format(mess, 'b').zfill(length)
-
-    def send_answer(self, connect, message):
+    def makeNewTime(self):
         time = datetime.now(timezone.utc).time()
-        name_encode = 'TCP-server'.encode('utf-8')
-        message_encode = message.encode('utf-8')
-        message_bit  = self.bit_zfill(time.hour, 5) + self.bit_zfill(time.minute, 6) + self.bit_zfill(time.second, 6)
-        message_bit += (self.bit_zfill(len(name_encode), 6) + format(False, '1b'))
-        message_send = b''
-        for i in range(0, 24, 8):
-            message_send += int(message_bit[i:(i + 8)], 2).to_bytes(1, byteorder = 'big')
-        message_send += (name_encode + message_encode)
-        try:
-            connect.sendall(message_send)
-        except ConnectionError as e:
-            pass
-        return
+        newData = self.message2Bit(time.hour, 5) + self.message2Bit(time.minute, 6) + self.message2Bit(time.second, 6)
+        return newData
 
-    def makeNewData(self, data, message):
-        name_encode = 'TCP-server'.encode('utf-8')
-        message_encode = message.encode('utf-8')
-        message_bit = data[:17] + self.bit_zfill(len(name_encode), 6) + format(False, '1b')
-        dataNew = b''
-        for i in range(0, 24, 8):
-            dataNew += int(message_bit[i:(i + 8)], 2).to_bytes(1, byteorder = 'big')
-        dataNew += (name_encode + message_encode)
+    def makeNewDataWithoutTime(self, dataTime, name, message):
+        messageBit = dataTime + self.message2Bit(len(name), 6) + format(False, '1b')
+        dataNew = int(messageBit, 2).to_bytes(3, byteorder = 'big') + name.encode(self.ENCODING) + message.encode(self.ENCODING)
         return dataNew
 
-    def stream_parser(self, data, connect):
-        data_hex = data.hex()
-        data_bin = (bin(int(data_hex[0], 16))[2:]).zfill(4)
-        for i in range(1, len(data_hex)):
-            data_bin += (bin(int(data_hex[i], 16))[2:]).zfill(4)
-        length_name = self.bitToInt(data_bin[17:23]) * 8 
-        name = self.byte_decode(data_bin[24:(24 + length_name)])    
-        # print(connect.getsockname())
+    def clientErrorDelete(self, name, dataTime, clientError):
+        dataNew = self.makeNewDataWithoutTime(dataTime, name, '-q')
+        self.stream_parser(dataNew, self.CLIENTS[name], clientError)
+        return
+
+    def sendMessage(self, connect, name, message):
+        try:
+            connect.sendall(message)
+        except ConnectionError as e:
+            print(f'\n   Warning! Lost connection with <{name}>!')
+            return 1
+        return 0
+
+    def stream_parser(self, data, connect, nameError = []):
+        length_name = ((int.from_bytes(data[2:3], byteorder = 'big') % 128) >> 1) + 3
+        name = data[3:length_name].decode(self.ENCODING)
+        message = data[length_name:].decode(self.ENCODING)
         if name in self.CLIENTS.keys() and self.CLIENTS[name] != connect:
-            self.send_answer(connect, 'ERROR-Name')
-            return
+            dataNew = self.makeNewDataWithoutTime(self.makeNewTime(), self.NAME, 'ERROR-Name')
+            self.sendMessage(connect, name, dataNew)
+            return True
+        dataTime = bin(int.from_bytes(data[:3], byteorder = 'big') >> 7)[2:]
         dataNew = data
         if name not in self.CLIENTS.keys():
             self.CLIENTS.setdefault(name, connect)
-            dataNew = self.makeNewData(data_bin, f'\t <{name}> joined the chat!')
+            dataNew = self.makeNewDataWithoutTime(dataTime, self.NAME, f'<{name}> joined the chat!')
+        flagWork = True
         if message.lower() in ['-q', 'quit', 'exit']:
+            flagWork = False
+            if name in nameError:
+                dataNew = self.makeNewDataWithoutTime(dataTime, self.NAME, f'Lost connection with <{name}>!')
+            else:
+                dataNew = self.makeNewDataWithoutTime(dataTime, self.NAME, f'<{name}> exited from the chat!')
+                self.sendMessage(self.CLIENTS[name], name, dataNew)
             self.CLIENTS.pop(name)
-            dataNew = self.makeNewData(data_bin, f'\t <{name}> exited from the chat!')
-        # print(dataNew)
-        for client in self.CLIENTS:
-            if client != 'TCP-server':
-                self.CLIENTS[client].sendall(dataNew)
-        return
+        clientError = []
+        for client in self.CLIENTS.keys():
+            if client != 'TCP-server' and client not in nameError:
+                if self.sendMessage(self.CLIENTS[client], client, dataNew):
+                    clientError.append(client)
+        for name in clientError:
+            self.clientErrorDelete(name, dataTime, clientError)
+        return flagWork
 
     def thread_transmitter(self, connect):
-        data_full = b''
-        while True:
-            try:
-                data = connect.recv(255)
-                if data:
-                    connect.settimeout(2)
+        flagWork = True
+        while flagWork:
+            data_full = b''
+            while True:
+                try:
+                    data = connect.recv(255)
                     data_full += data
-            except socket.timeout: break
-        if len(data_full) > 0: self.stream_parser(data_full, connect)
+                    if len(data) < 255:
+                        break
+                except ConnectionError as e:
+                    flagWork = False
+                    print(f'\n   Warning! Lost connection...!')
+                    break
+            if len(data_full) > 0:
+                flagWork = self.stream_parser(data_full, connect)
         return
 
     def main(self):
         thread_transm = []
-        print('\n\t TCP server ready!\n')
+        print('\n\t TCP server (sync) ready!\n')
         while True:
-            try:
-                # input() 
-                conn, addr = self.SOCKET.accept()
-                transmitter = threading.Thread(target = self.thread_transmitter, args = (conn,))
+            try: 
+                connect, addr = self.SOCKET.accept()
+                transmitter = threading.Thread(target = self.thread_transmitter, args = (connect,))
                 transmitter.start()
                 thread_transm.append(transmitter)
             except KeyboardInterrupt:
@@ -118,4 +120,4 @@ class TCP_chat_sync_server():
         return 0
     
 if __name__ == '__main__':
-    TCP_chat_sync_server()
+    TCP_chat_sync_server(PORT, ENCODING)
